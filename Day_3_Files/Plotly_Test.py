1143d2c6b18c0433edf491ddb57c98d4af375dc4
c-westhoven <charlotte@westhoven.com>
2021-02-03 22:06:46 +0100

    made a more general plt

--- Day_3_Files/Plotly_Test.py
+++ Day_3_Files/Plotly_Test.py
@@ -3,6 +3,7 @@ import plotly.graph_objects as go
 import pandas as pd
 from collections import deque
 
+
 def sequence_from_fasta(fastafile):
     with open(fastafile) as fasta:
         combined_seq = ""
@@ -35,7 +36,8 @@ def hydropathy_sequence_list(sequence, mapping_dict):
     return sequence_as_hydropathy
 
 
-def plot_sequence_bar(sequence_aa, sequence_hydropathy):
+def plot_sequence_bar(sequence_aa, sequence_hydropathy, title="", xaxis="", yaxis=""):
+
     sequence_aa_list=[]
     for pos, aminoacid in enumerate(sequence_aa):
         sequence_aa_list.append(aminoacid + str(pos))
@@ -46,18 +48,18 @@ def plot_sequence_bar(sequence_aa, sequence_hydropathy):
         )
     ]
     fig = go.Figure(data=data)
-    fig.update_layout(title_text='Hydropathy Along G Protein Sequence',
+    fig.update_layout(title_text= title,
                       xaxis=dict(
-                          title='G Protein Sequence'
+                          title= xaxis
                       ),
                       yaxis=dict(
-                          title='Hydropathy'
+                          title= yaxis
                       ))
     fig.show()
     return
 
 
-def plot_sequence_bubble(sequence_aa, sequence_hydropathy):
+def plot_sequence_bubble(sequence_aa, sequence_hydropathy, title="", xaxis="", yaxis=""):
     sequence_aa_list=[]
     for pos, aminoacid in enumerate(sequence_aa):
         sequence_aa_list.append(aminoacid + str(pos))
@@ -74,12 +76,12 @@ def plot_sequence_bubble(sequence_aa, sequence_hydropathy):
         )
     ]
     fig = go.Figure(data=data)
-    fig.update_layout(title_text='Hydropathy Along G Protein Sequence',
+    fig.update_layout(title_text= title,
                       xaxis=dict(
-                          title='G Protein Sequence'
+                          title= xaxis
                       ),
                       yaxis=dict(
-                          title='Hydropathy'
+                          title= yaxis
                       ))
     fig.show()
     return
@@ -119,10 +121,39 @@ def plot_sequence_bar_window(len_window_list, sequence_hydropathy):
     return
 
 
+def plot_bar(seq_aa=None, seq_hydropathy=list, title="", xaxis="", yaxis=""):
+    if isinstance(seq_aa, str) == True:
+        sequence_aa_list=[]
+        for pos, aminoacid in enumerate(seq_aa):
+            sequence_aa_list.append(aminoacid + str(pos))
+    elif seq_aa is None:
+        sequence_aa_list = []
+        for x in range(len(seq_hydropathy)):
+            sequence_aa_list.append(x)
+    else:
+        sequence_aa_list = seq_aa
+    data = [
+        go.Bar(
+            x=sequence_aa_list,
+            y=seq_hydropathy
+        )
+    ]
+    fig = go.Figure(data=data)
+    fig.update_layout(title_text= title,
+                      xaxis=dict(
+                          title= xaxis
+                      ),
+                      yaxis=dict(
+                          title= yaxis
+                      ))
+    fig.show()
+    return
+
+
 if __name__ == '__main__':
-    sequence = sequence_from_fasta("./P32249.fasta")
+    seq = sequence_from_fasta("./P32249.fasta")
     mapping_dict = mapping_dict("../data/amino_acid_properties.csv")
-    sequence_hydropathy = hydropathy_sequence_list(sequence, mapping_dict)
-    # plot_sequence_bubble(sequence, sequence_hydropathy)
-    window = sliding_window_hydropathy(sequence, mapping_dict, 10)
-    plot_sequence_bar_window(len(window), window)
+    sequence_hydropathy = hydropathy_sequence_list(seq, mapping_dict)
+    plot_bar(seq, seq_hydropathy=sequence_hydropathy, title="Hydropathy Along G Protein Sequence", xaxis="G Protein Sequence", yaxis="Hydropathy")
+    window = sliding_window_hydropathy(seq, mapping_dict, 10)
+    plot_bar(seq_hydropathy=window)

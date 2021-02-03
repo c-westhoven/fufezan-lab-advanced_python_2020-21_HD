ef2011a984fc2e341e7e560ca9d774c642a306c3
c-westhoven <charlotte@westhoven.com>
2021-02-03 14:28:07 +0100

    made into function

--- notebooks/Exercise_2_Histogram.py
+++ notebooks/Exercise_2_Histogram.py
@@ -80,3 +80,25 @@ plt.ylabel("counts")
 plt. title("Lion")
 plt.bar(list(counted_seq.keys()), list(counted_seq.values()))
 plt.savefig("../Results_Ex_2/Lion_Westhoven.pdf")
+
+# as a function
+def make_histogram_from_fasta(fasta_file, name_of_organism):
+    with open(fasta_file) as aap:
+        combined_seq = ""
+        for line_dict in aap:
+            if not line_dict.startswith(">"):
+                combined_seq += line_dict.replace("\n", "")
+    counted_seq = dict(Counter(combined_seq))
+
+    plt.xlabel("aa")
+    plt.ylabel("counts")
+    plt. title(str(name_of_organism))
+    plt.bar(list(counted_seq.keys()), list(counted_seq.values()))
+    plt.savefig("../Results_Ex_2/" + str(name_of_organism)+ ".pdf")
+
+if __name__ == '__main__':
+    lion = make_histogram_from_fasta("../uniprot_Panthera_leo_Lion.fasta", "lion")
+    eggplant = make_histogram_from_fasta("../uniprot_Solanum_melongena_Eggplant.fasta", "eggplant")
+    human = make_histogram_from_fasta("../uniprot-filtered-organism__Homo+sapiens+(Human)+[9606]_+AND+review--.fasta", "human")
+    bacillus = make_histogram_from_fasta("../uniprot_bacillus_subtilis.fasta", "bacillus")
+    archaea = make_histogram_from_fasta("../uniprot_Haloquadratum_walsbyi.fasta", "archaea")

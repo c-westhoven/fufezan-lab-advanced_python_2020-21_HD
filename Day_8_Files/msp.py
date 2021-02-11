b377898c1d7580b1b45e345e6781f09baae0c2f8
c-westhoven <charlotte@westhoven.com>
2021-02-11 23:52:31 +0100

    df, and seqs good, no figuring out timeit

--- Day_8_Files/msp.py
+++ Day_8_Files/msp.py
@@ -46,72 +46,31 @@ def msp_to_df(
         seqs = pd.DataFrame()
         seqs["seq"] = seq_list
 
-        mz_int = []
+        dict = {}   # empty dict
         prefixes = ("M", "N", "C")
-        for idx, value in enumerate(list_spectra):
+        for idx_sequence, value in enumerate(list_spectra):
             row_spectra = value.split("\n")
             row_spectra = [x for x in row_spectra if not x.startswith(prefixes)]    # list with mz and ints [mzintblah, mzintblah, mzintblah, ...]
+            dict[idx_sequence] = {}
             for idx, value in enumerate(row_spectra):
                 mz_split = row_spectra[idx].split("\t")     # list [mz , int , blah]
-                mz_val = round(float(mz_split[0]))
-                int_val = round(float(mz_split[1]))
+                mz_val = round(float(mz_split[0]))      # rounded mz
+                int_val = float(mz_split[1])     # unrounded intensity
                 if mz_val > mz_max or mz_val < mz_min:
                     continue
+                elif mz_val in dict[idx_sequence].keys():
+                    if int_val > dict[idx_sequence][mz_val]:
+                        dict[idx_sequence][mz_val] = int_val
                 else:
-                    mz_int.append([mz_val, int_val])
+                    dict[idx_sequence][mz_val] = int_val
 
-
-    df = None
+    df = pd.DataFrame(dict).T
     return df, seqs
 
 
 if __name__ == '__main__':
     msp_file = "./cptac2_mouse_hcd_selected.msp"
     df, seq = msp_to_df(msp_file)
-    print(df, seq)
+    # print(df, seq)
+    print(timeit.timeit("msp_to_df()", setup="from __main__ import msp_to_df"))
 
-    # with open(msp_file) as file:
-    #
-    #     file = file.read()
-    #     list_spectra = file.split("\n\n")
-    #     list_spectra = list_spectra[:-1]
-    #
-    #     prefixes = ("M", "N", "C")
-    #     for idx, value in enumerate(list_spectra):
-    #         row_spectra = value.split("\n")
-    #         row_spectra = [x for x in row_spectra if not x.startswith(prefixes)]
-        #     for idx, value in enumerate(row_spectra):
-        #         if not value[0] == "N":
-        #             continue
-        #         elif not value[0] == "M":
-        #             continue
-        #         elif not value[0] == "C":
-        #             continue
-        #         else:
-        #             row_spectra.pop(idx)
-        # print(row_spectra)
-
-    # with open(msp_file) as file:
-    #     file = file.read()
-    #     list_spectra = file.split("\n\n")
-    #     list_spectra = list_spectra[:-1]
-    #     df_list = []
-    #     max_seq_len = 30
-    #     min_seq_len = 1
-    #     min_ce = 36
-    #     max_ce = 40
-    #     for idx, value in enumerate(list_spectra):
-    #         # value = list_spectra[idx]
-    #         seq = value[value.find(" ")+1:value.find("/")]
-    #         ce_split = value.split("\n")[0]
-    #         ce_ev = ce_split.split("_")[-1]
-    #         ce = float(ce_ev[:ce_ev.find("eV")])
-    #         print(ce)
-    #         if len(seq) > max_seq_len or ce > max_ce or min_ce > ce:
-    #             list_spectra.pop(idx)
-    #         else:
-    #             df_list.append(seq)
-    #             df_list.append(ce)
-    #
-    #
-    # print(df_list, len(df_list))

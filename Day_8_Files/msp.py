9c5f764860eea67419557849d065e228e3ef5c87
c-westhoven <charlotte@westhoven.com>
2021-02-11 23:15:42 +0100

    got mz into list

--- Day_8_Files/msp.py
+++ Day_8_Files/msp.py
@@ -45,31 +45,50 @@ def msp_to_df(
 
         seqs = pd.DataFrame()
         seqs["seq"] = seq_list
-        mz = []
+        mz_int = []
+        prefixes = ("M", "N", "C")
         for idx, value in enumerate(list_spectra):
             row_spectra = value.split("\n")
+            row_spectra = [x for x in row_spectra if not x.startswith(prefixes)]    # list with mz and ints [mzint, mzint, mzint, ...]
             for idx, value in enumerate(row_spectra):
-                if value.startswith("N") and value.startswith("M") and value.startswith("C"):
-                    row_spectra.pop(idx)
-            # if value.startswith("N") or value.startswith("M") or value.startswith("C"):
-            #     row_spectra.pop(idx)
-                    mz_split = row_spectra.split("\t")
-                    for idx, value in enumerate(mz_split):
-                        mz_int = mz_split[idx]
-                        mz_val = mz_int[mz_int.rfind("\n")+1:]
-                        print("val", mz_val)
-                        mz.append(mz_val)
-        print("list", mz)
+                mz_split = row_spectra[idx].split("\t")     # list [mz , int , blah]
+                mz_val = round(float(mz_split[0]))
+                int_val = round(float(mz_split[1]))
+                if mz_val > mz_max or mz_val < mz_min:
+                    continue
+                else:
+                    mz_int.append([mz_val, int_val])
+
 
     df = None
     return df, seqs
-
 
 
 if __name__ == '__main__':
     msp_file = "./cptac2_mouse_hcd_selected.msp"
     df, seq = msp_to_df(msp_file)
     print(df, seq)
+
+    # with open(msp_file) as file:
+    #
+    #     file = file.read()
+    #     list_spectra = file.split("\n\n")
+    #     list_spectra = list_spectra[:-1]
+    #
+    #     prefixes = ("M", "N", "C")
+    #     for idx, value in enumerate(list_spectra):
+    #         row_spectra = value.split("\n")
+    #         row_spectra = [x for x in row_spectra if not x.startswith(prefixes)]
+        #     for idx, value in enumerate(row_spectra):
+        #         if not value[0] == "N":
+        #             continue
+        #         elif not value[0] == "M":
+        #             continue
+        #         elif not value[0] == "C":
+        #             continue
+        #         else:
+        #             row_spectra.pop(idx)
+        # print(row_spectra)
 
     # with open(msp_file) as file:
     #     file = file.read()
